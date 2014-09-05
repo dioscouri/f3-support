e8a72d200a0a4d7ad42f05c7399af8ca8358b4ce
Rafael Diaz-Tushman <rdiaztushman@dioscouri.com>
2014-09-05 17:48:21 -0400

    open live chat tab on live chat landing page, fixes dioscouri/amritasingh-f3#452

--- src/Support/Site/Views/livechat/index.php
+++ src/Support/Site/Views/livechat/index.php
@@ -1,2 +1,8 @@
 <?php $settings = \Support\Models\Settings::fetch(); ?>
 <div><?php echo $settings->live_chat_index; ?></div>
+
+<script>
+jQuery(window).load(function(){
+    jQuery('#site-chat-body').collapse('show');
+});
+</script>
